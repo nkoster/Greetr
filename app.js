d9368d4d3d62b2e76a87e207ed9b94c54f70f566
Niels Koster <n.koster@portavita.eu>
2018-10-10 00:50:50 +0200

    Test class, and chain all.

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-var g = $G('Niels');
-g.greet().setLanguage('en').greet(true).setLanguage('nl').log();
-g.setGreeting('#greeting', true);
+var g = $G('Niels', 'Apekop', 'nl');
+g.greet().setLanguage('en').greet(true).setLanguage('nl').log()
+.setGreeting('#greeting', true).setGreeting('.g2');
