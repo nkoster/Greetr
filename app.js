21723461903af75fe8cd407d86cac0a2a1168c94
Niels Koster <n.koster@portavita.eu>
2018-10-11 01:07:33 +0200

    Click example, chaining the function calls

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+// get a new object. The architecture allows us to
+// not have to use the "new" keyword here
 var g = $G('Niels', 'Apekop', 'nl');
+
+// use the chainable methods
 g.greet().setLanguage('en').greet(true).setLanguage('nl').log()
 .setGreeting('#greeting', true).setGreeting('.g2');
+
+// change the g object with a click event
+$('#login').click(function() {
+    $('#logindiv').hide();
+    // use the chainable methods to change
+    // everything on the fly
+    g.setLanguage($('#language').val())
+        .setGreeting('#greeting', true)
+        .setGreeting('.g2')
+        .log()
+});
